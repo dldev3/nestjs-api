57a9a9a5370633432a5e57f3f637bac99062b9b2
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-03-05 00:17:29 +0530

    prima & db added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable({})
 export class AuthService {
-    login() {
-
+    signup() {
+        return { msg: 'I have signed up!' }
     }
 
-    signup() {
-
+    signin() {
+        return { msg: 'I have signed in!' }
     }
 }

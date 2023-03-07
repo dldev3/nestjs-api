3c86468fc99f3a657868988fb52933e2c7f6d850
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-03-07 23:31:31 +0530

    jwt added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,18 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+    ) { }
+
     async signup(dto: AuthDto) {
         // generate the password hash
         const hash = await argon.hash(dto.password);
@@ -20,9 +27,7 @@ export class AuthService {
                     hash,
                 },
             });
-            delete user.hash;
-            //return the saved user
-            return user;
+            return this.signToken(user.id, user.email);
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
@@ -48,9 +53,28 @@ export class AuthService {
         //if password incorrect, throw exception
         if (!pwMatches) throw new ForbiddenException('Credential incorrect');
         //send back the user
-        delete user.hash;
-        return user;
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+
+        const secret = this.config.get("JWT_SECRET")
+
+        const token = await this.jwt.signAsync(
+            payload,
+            {
+                expiresIn: '15m',
+                secret: secret
+            },
+        );
+
+        return {
+            access_token: token
+        };
     }
 }
 
-//1.21.26

7e6251a77c32f50e757d0896fb6a4b918924bf3b
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-03-08 23:27:58 +0530

    user module done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
     @Get('me')
-    getMe() {
-        return "user info";
+    getMe(@GetUser() user: User) {
+        return user;
     }
 }
+
+//1.57.18
